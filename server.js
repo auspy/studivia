8629a008fae61e293e1d1e6cacc545bd3dc1fb41
Kshetez Vinayak <spark@Kshetezs-MacBook-Air.local>
2022-03-30 01:44:35 +0530

    change sql connection

--- server.js
+++ server.js
@@ -32,20 +32,19 @@ app.use(bodyParser.urlencoded({
 })) // url encoded is used to get information from html forms
 
 // CREATE CONNECTION
-// sql code: karne_kya_he kispe_karna_he uska_nam_kya_he usme_kya_kya_hoga
-// var con = mysql.createConnection({
-//     host: "studiviadb.cnreudh8065g.ap-south-1.rds.amazonaws.com",
-//     port: "3306",
-//     user: "auspy",
-//     password: "password",
-//     database: "studiviaDB"
-// })
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: "studiviadb.cnreudh8065g.ap-south-1.rds.amazonaws.com",
+    port: "3306",
+    user: "auspy",
     password: "password",
     database: "studiviaDB"
 })
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "studiviaDB"
+// })
 
 // CONNECT
 con.connect(function (err) {
